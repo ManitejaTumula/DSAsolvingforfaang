787c9e198b35579a011ef534ea1a1a23ba446211
mani <84654396+ManitejaTumula@users.noreply.github.com>
2026-07-06 18:24:34 +0530

    Time: 10 ms (14.29%), Space: 21.4 MB (14.87%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,23 +1,25 @@
-from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-      graph=[[] for _ in range(numCourses)]
-      indegree = [0]*numCourses
+        graph={i:[] for i in range(numCourses)}
+        for course,preq in prerequisites:
+            graph[course].append(preq)
+        visited=set()
+        def dfs(course):
+            if course in visited:
+                return False
+            if graph==[course]:
+                return True
+            visited.add(course)
+            for preq in graph[course]:
+                if not dfs(preq): return False
+            visited.remove(course)
+            graph[course]=[]
+            return True
+        for course in range(numCourses):
+            if not dfs(course):return False
+        return True
 
-      for course,prereq in prerequisites:
-        graph[prereq].append(course)
-        indegree[course]+=1
-      queue=deque()
-      for i in range(numCourses):
-        if indegree[i]==0:
-            queue.append(i)
-        finish=0
-      while queue:
-        curr=queue.popleft()
-        finish+=1
-        for course in graph[curr]:
-            indegree[course]-=1
-            if indegree[course]== 0:
-                queue.append(course)
-      return finish==numCourses
+
+
+
 
