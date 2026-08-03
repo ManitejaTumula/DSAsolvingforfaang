1ce9c83e640b56f7e04a77ae3caec794f37d1d61
mani <84654396+ManitejaTumula@users.noreply.github.com>
2026-08-03 12:25:49 +0530

    Time: 0 ms (100%), Space: 19.2 MB (52.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,10 @@
 class Solution:
         def climbStairs(self, n: int) -> int:
-            #Using memoization
-            memo=[-1] * (n+1)
-            def check(i):
-                if i==0 or i==1:
-                    return 1
-                if memo[i]!=-1:
-                    return memo[i]
-                memo[i]=check(i-1) + check(i-2)
-                return memo[i]
-            return check(n)
-        
+            if n==1:
+                return 1
+            dp = [0] * (n+1)
+            dp[1]=1
+            dp[2]=2
+            for i in range(3,n+1):
+                dp[i]=dp[i-1] + dp[i-2]
+            return dp[n]
