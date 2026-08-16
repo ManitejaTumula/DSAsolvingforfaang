b3924126e51f568828ec7ffa2e08581e605efcaf
mani <84654396+ManitejaTumula@users.noreply.github.com>
2026-08-16 20:44:53 +0530

    Time: 43 ms (88.55%), Space: 30.6 MB (44.27%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        counter=[0,0,0]
+        total=0
+        for stone in stones:
+            counter[stone%3]+=1
+        c0,c1,c2=counter[0],counter[1],counter[2]
+
+        if c0 % 2 ==0:
+            return c1>=1 and c2>=1
+        return abs(c1-c2) > 2
+        
